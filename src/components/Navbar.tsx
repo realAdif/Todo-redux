e9f44e9c7672889680c22271dda55e0792d62c2b
Aditya <90451018+realAdif@users.noreply.github.com>
2024-03-24 19:09:52 +1100

    Add dark mode toggle button to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,24 @@
 import sun_icon from '../assets/icon-sun.svg';
+import moon_icon from '../assets/icon-moon.svg';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/slice/themeSlice';
+import { RootState } from '../redux/store/store';
 
 function Navbar() {
+  const dispatch = useDispatch();
+  const handleToggleTheme = () => {
+    dispatch(toggleTheme());
+  };
+  const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
+  const icon = isDarkMode ? moon_icon : sun_icon;
+
   return (
     <div className="text-white flex justify-between items-center my-4">
       <h1>TODO</h1>
-      <img src={sun_icon} alt="sun_icon" />
+      <button onClick={handleToggleTheme}>
+        <img src={icon} alt="icon" />
+      </button>
     </div>
   );
 }
