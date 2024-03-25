7860aeddc901a0a8ede2a2f1403efcfcc298dcf8
Aditya <90451018+realAdif@users.noreply.github.com>
2024-03-25 13:09:07 +1100

    Fix positioning of elements in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import ViewTask from './components/ViewTask';
 function App() {
   return (
     <main>
-      <div className=" relative">
+      <div className="relative">
         <Header />
-        <div className=" w-full max-w-lg  mx-auto px-4 absolute -bottom-10 left-1/2 transform -translate-x-1/2 ">
+        <div className=" w-full max-w-lg  mx-auto px-4 absolute top-20  left-1/2 transform -translate-x-1/2 ">
           <Navbar />
           <CreateTask />
           <ViewTask />
